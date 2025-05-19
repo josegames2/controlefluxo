c5da1f6d577c20ae7ae1d2d346dda720efcabba9
Jose Dantas <131831675+josegames2@users.noreply.github.com>
2025-05-19 16:36:48 -0300

    bn

--- DesafioControleFluxo.java
+++ DesafioControleFluxo.java
@@ -3,17 +3,30 @@ import java.util.Scanner;
 public class DesafioControleFluxo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1;
-        int n2;
-       do {
-               System.out.println("digite o priemiro numero");
-               n1 = sc.nextInt();
 
-               System.out.println("digite o segundo numero");
-               n2 = sc.nextInt();
-               if (n1 > n2){
-                   System.out.println("o segundo numero dever ser maior que o primeiro");
-               }
-       }while(n1 < n2);
+        System.out.println("digite o priemiro numero");
+        int n1 = sc.nextInt();
+
+        System.out.println("digite o segundo numero");
+        int n2 = sc.nextInt();
+
+        try {
+            contar(n1,n2);
+        } catch (ParametrosInvalidosException e) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+        sc.close();
     }
+        static class ParametrosInvalidosException extends Exception {}
+
+        public static void contar(int n1, int n2) throws ParametrosInvalidosException{
+           if(n1 > n2){
+                throw new ParametrosInvalidosException();
+           }
+            int contagem = n2 - n1;
+            for (int i = 0 ;i < contagem; i++){
+                System.out.println(i);
+            }
+        }
+
 }
