d036b930818c9cf30573e57833d6bb6abaec8030
Jose Dantas <131831675+josegames2@users.noreply.github.com>
2025-05-19 17:24:17 -0300

    agora eu aprendi

---
+++ CalcularExtends.java
@@ -0,0 +1,16 @@
+public class CalcularExtends{
+    public static class NumeroMaior extends Exception{}
+
+
+    public static void contar(int n1,int n2) throws NumeroMaior{
+        if (n1 > n2 ){
+            throw new NumeroMaior();
+        }
+        int contagem = n2 - n1;
+        for (int i = 1; i < contagem; i++){
+            System.out.println(i);
+        }
+    }
+}
+
+
--- DesafioControleFluxo.java
+++ DesafioControleFluxo.java
@@ -11,22 +11,11 @@ public class DesafioControleFluxo {
         int n2 = sc.nextInt();
 
         try {
-            contar(n1,n2);
-        } catch (ParametrosInvalidosException e) {
+            CalcularExtends.contar(n1,n2);
+        } catch (CalcularExtends.NumeroMaior e ) {
             System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
         sc.close();
     }
-        static class ParametrosInvalidosException extends Exception {}
-
-        public static void contar(int n1, int n2) throws ParametrosInvalidosException{
-           if(n1 > n2){
-                throw new ParametrosInvalidosException();
-           }
-            int contagem = n2 - n1;
-            for (int i = 0 ;i < contagem; i++){
-                System.out.println(i);
-            }
-        }
 
 }
